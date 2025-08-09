d6400c90ea27040ea6e62e5ad238c94b2394edaa
Kerry Pan <kerrypan25@gmail.com>
2025-08-08 23:22:39 -0400

    deleted old files

--- Loss.py
+++
@@ -1,9 +0,0 @@
-def masked_mse_loss(pred, target, building_mask, eps=1e-8):
-    diff_sq = (pred - target).pow(2)
-    non_build = (building_mask < 0.5).float() 
-
-    masked_diff = diff_sq * non_build
-    sums = masked_diff.sum(dim=[1, 2, 3]) 
-    counts = non_build.sum(dim=[1, 2, 3]).clamp_min(eps) 
-
-    return (sums / counts).mean()
